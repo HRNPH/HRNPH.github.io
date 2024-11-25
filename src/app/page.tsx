a946165cfb4c0ccb3a3231e748fe897ebfb8752b
HRNPH <hrnph@protonmail.com>
2024-11-25 13:49:09 +0700

    feat: Update Home component to include title and configure WaifuDisplayer model options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,26 @@ import WaifuDisplayer from "@/components/live2d/WaifuDisplayer";
 export default function Home() {
   return (
     <div className="container">
-      <WaifuDisplayer />
+      <div className="h-screen">
+        <div className="text-center my-4">
+          <h1 className="font-bold text-4xl">Hirunkul Phimsiri</h1>
+        </div>
+        <WaifuDisplayer
+          className="absolute top-0 right-0 h-screen w-2/6"
+          modelOptions={{
+            model: "/models/SakiUnit/02saki_unit.model3.json",
+            position: {
+              x: 0.4,
+              y: 0.4,
+            },
+            scale: {
+              x: 0.5,
+              y: 0.5,
+            },
+            alpha: 0,
+          }}
+        />
+      </div>
     </div>
   );
 }
