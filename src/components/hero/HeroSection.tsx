77b77a93f4da2cd18a1e99aa3bcce4048ec3339c
HRNPH <hrnph@protonmail.com>
2024-12-29 08:39:08 +0700

    feat: Update HeroSection with enhanced personal introduction and background details

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -23,14 +23,27 @@ const HeroSection = () => {
             .
           </h1>
           <TypeWritingBio />
-          <p className="text-lg text-gray-300">
-            A passionate{" "}
+          <p className="text-base text-gray-300">
             <span className="font-medium bg-gradient-to-r from-blue-300 to-purple-400 bg-clip-text text-transparent">
-              Developer
-            </span>
-            , building innovative web experiences and solving challenging
-            problems with modern technologies.
+              Welcome to my personal website!
+            </span>{" "}
+            Hi, I’m Guide, an ML Engineer and Full-Stack Developer. My passion
+            lies in creating systems that address real-world challenges through
+            ML/DL. From developing AI tools like chatbots and RAG systems to
+            deploying them in production, I focus on building complete,
+            practical solutions. With experience in web development, cloud
+            platforms, and CI/CD pipelines, I can craft tailored solutions and
+            turn them into functional products or services.
           </p>
+          <p className="text-base text-gray-300">
+            Beyond technical work, I’ve co-founded a startup and led projects
+            that merge technology with business, giving me hands-on experience
+            in transforming ideas into impactful products. Currently studying
+            Computer Engineering at Chulalongkorn University, I actively
+            contribute to open-source projects and enjoy collaborating to create
+            meaningful technology.
+          </p>
+
           <div className="flex flex-col sm:flex-row gap-4 sm:space-x-4 items-center justify-center lg:justify-start">
             <Button className="w-full sm:w-auto px-6 py-3 text-lg bg-purple-500 hover:bg-purple-600 text-white transition-colors duration-300">
               Talk With My Secretary
